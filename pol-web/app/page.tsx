edb768ed2046e3349266ba0436d6e9e4ae683261
Asahi <u444067@st.pu-toyama.ac.jp>
2025-05-29 16:23:17 +0900

    ファイル追加

--- pol-web/app/page.tsx
+++ pol-web/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div>
       <h1>Polygon Web</h1>
       <p>ああああああいいいいいいい</p>
+      <li>
+        <Link href="/auther">
+        auther 1
+        </Link>
+      </li>
 
     </div>
   );
